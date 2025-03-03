3037d73e597a203b8b6eae93e42a6e3447f326b9
Scott Spuler <scott@onehat.com>
2025-03-03 11:13:00 -0800

    Update DuplicatableBehavior.php with duplicatedModel

--- src/Model/Behavior/DuplicatableBehavior.php
+++ src/Model/Behavior/DuplicatableBehavior.php
@@ -53,7 +53,7 @@ class DuplicatableBehavior extends Behavior
     {
         return $this->_table->save(
             $this->duplicateEntity($id),
-            $this->getConfig('saveOptions') + ['associated' => $this->getConfig('contain'), 'duplicated' => true, 'originalId' => $id]
+            $this->getConfig('saveOptions') + ['associated' => $this->getConfig('contain'), 'duplicated' => true, 'originalId' => $id, 'duplicatedModel' => $this->_table->getAlias()]
         );
     }
 
